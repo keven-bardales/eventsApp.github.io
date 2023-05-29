2dd31290dc115f483208821e375f76365a1c6c2e
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-05-29 15:48:29 -0600

    update

--- src/controllers/locations.controller.js
+++ src/controllers/locations.controller.js
@@ -2,9 +2,7 @@ import { pool } from '../dataBase.js';
 
 export const getLocations = async (req, res) => {
   try {
-    const [rows] = await pool.query(
-      'SELECT * FROM db_event_agenda.tbl_locations'
-    );
+    const [rows] = await pool.query('SELECT * FROM tbl_locations');
     res.json(rows);
   } catch (err) {
     res.status(500).json({ error: 'Something went wrong in the server side' });
