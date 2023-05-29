4093c21f3d3cc33758d8673f829a372c6c55eb6d
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-05-29 00:32:28 -0600

    update

--- src/dataBase.js
+++ src/dataBase.js
@@ -4,6 +4,7 @@ import { DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, DATABASE } from './config.js';
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
+  port: DB_PORT,
   password: DB_PASSWORD,
   database: DATABASE,
 });
