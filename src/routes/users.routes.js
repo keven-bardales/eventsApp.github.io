b194ed8512c91901c415d360ad2913cb43706073
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-05-29 00:29:47 -0600

    udapte

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,7 +8,7 @@ import {
 } from '../controllers/users.controllers.js';
 const router = Router();
 
-router.get('/users', getUsers);
+router.get('/users', res.send('Hola Mundo'));
 
 router.get('/users/:id', getUserById);
 
